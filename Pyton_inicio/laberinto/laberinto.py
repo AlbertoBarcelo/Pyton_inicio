dd630208aad70c1997a736e424b3d1b8cc59670d
AlbertoBarcelo <albertobd6@gmail.com>
2023-02-13 17:52:51 +0100

    13-02-23

--- Pyton_inicio/laberinto/laberinto.py
+++ Pyton_inicio/laberinto/laberinto.py
@@ -1,13 +1,48 @@
 # resolver un laberinto 
 # hay que devolver dos listas en plan abajo derecha con las direcciones que has recorrido y luego con las coordenadas
-
 import os
+import colorama
 # lo primero que hacemps es limpiar las consola
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
+
+def laberinto():
+    laberinto = [
+        ['e', '1', '1', '1', '1'],
+        ['0', '0', '0', '1', '1'],
+        ['0', '0', '0', '0', '1'],
+        ['1', '1', '0', '0', '0'],
+        ['1', '1', '0', '1', 's'],
+    ]
+    return laberinto
+
+def pintar(array):
+    for i in range(5):
+        for j in range(5):
+            if [j] == '0':
+                print(colorama.Back.CYAN + str(array[j][i]))
+            if [j] == '1':
+                print(colorama.Back.RED + str(array[j][i]))
+            if [j] == 'e':
+                print(colorama.Back.YELLOW + str(array[j][i]))
+            if [j] == 's':
+                print(colorama.Back.YELLOW + str(array[j][i]))
+    return array
+
+
+
+
+lab = laberinto()
+pintarlab =pintar(lab)
+print(pintarlab)
+
+
+
+
+
 
 # primero creamos el laberinto que es una matriz con posiciones en la cada posicion creamos un (" ") para luego poder definir los muros
 # para los muros recorremos el array y donde hay un ladrillo le ponemos una x
